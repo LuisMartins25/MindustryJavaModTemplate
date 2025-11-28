1a4b939fd33eab04f8e27f6aa09215f387c44e30
Arati Muraleedharan Puoommangalathu <a.puoommangalathu@campus.fct.unl.pt>
2025-11-28 00:38:58 +0000

    Stamina bar update and movement logic (NOT WORKING)

--- src/StaminaBoostMod/StaminaBoostMain.java
+++ src/StaminaBoostMod/StaminaBoostMain.java
@@ -2,16 +2,27 @@ package StaminaBoostMod;
 
 import arc.*;
 import arc.graphics.Color;
+import arc.input.KeyCode;
+import arc.math.Mathf;
 import arc.scene.ui.layout.Table;
 import arc.util.*;
 import mindustry.Vars;
+import mindustry.game.EventType;
 import mindustry.game.EventType.*;
+import mindustry.gen.Unit;
 import mindustry.mod.*;
 import mindustry.ui.Bar;
 
 public class StaminaBoostMain extends Mod {
+    private static final float STAMINA_DECREASE_RATE = 0.05f;
+    private static final float STAMINA_RECOVERY_RATE = 0.01f;
+    private static final float SPEED_INCREASE_RATE = 1.5f;
+    private static final float SPEED_DECREASE_RATE = 0.5f;
+    private static final float DEFAULT_SPEED = 1f; // TO CHECK the dafault speed in game
 
     public static float staminaLeft = 1.0f;
+    private static boolean lowStamina = false;
+
     public StaminaBoostMain(){
         Log.info("Loaded StaminaBoostMain constructor.");
 
@@ -19,11 +30,62 @@ public class StaminaBoostMain extends Mod {
             Table myTable = new Table();
             myTable.bottom();
             myTable.setFillParent(true);
-            Bar staminaBar = new Bar("Stamina", Color.orange, () -> staminaLeft);
+            Bar staminaBar = new Bar(() -> "Stamina", () -> blinkColor(), () -> staminaLeft);
             myTable.add(staminaBar).width(200f).height(25f).padBottom(60f);
             myTable.visible(() -> Vars.ui.hudfrag.shown);
             Vars.ui.hudGroup.addChild(myTable);
+
+            Events.run(EventType.Trigger.update, () -> update());
         });
+    }
+
+    private Color blinkColor(){
+        Color base = new Color(Color.red).lerp(Color.green, staminaLeft);
+        if(lowStamina){
+            float s = Mathf.absin(Time.time, 6f, 1f);
+            return base.lerp(Color.white, s);
+        }
+        return base;
+    }
+
+    private void update(){
+        if(Vars.player == null || Vars.player.unit() == null) return;
+
+        Unit unit = Vars.player.unit();
+        boolean isShiftPressed = Core.input.keyDown(KeyCode.shiftLeft);
+        boolean isMousePressed = Core.input.keyDown(KeyCode.mouseLeft);
+        float delta = Time.delta / 60f;
+
+        if(isShiftPressed && isMousePressed){
+            staminaDec(delta);
+            unit.speedMultiplier = SPEED_INCREASE_RATE;
+            if(staminaLeft <= 0.3){
+                unit.speedMultiplier = SPEED_INCREASE_RATE * SPEED_DECREASE_RATE;
+                lowStamina = true;
+            }
+        }
+        else{
+            staminaInc(delta);
+            unit.speedMultiplier = DEFAULT_SPEED;
+        }
+    }
+    private void staminaInc(float delta){
+        float stamina = staminaLeft + STAMINA_RECOVERY_RATE * delta;
+        if(stamina >= 1.0f)
+            staminaLeft = 1.0f;
+        else
+            staminaLeft = stamina;
+
+        if(staminaLeft > 0.3f)
+            lowStamina = false;
+    }
+
+    private void staminaDec(float delta){
+        float stamina = staminaLeft - STAMINA_DECREASE_RATE * delta;
+        if(stamina <= 0.0f)
+            staminaLeft = 0.0f;
+        else
+            staminaLeft = stamina;
     }
     @Override
     public void loadContent(){
